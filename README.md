e6c45101cb2186d2c0e0eadbd70780111ac47796
Mauricio Aizaga <mauricioaizaga@gmail.com>
2013-08-22 15:14:49 -0500

    Twitter bootstrap

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ django-crud-example
 ===================
 
 Django crud example
+
+
+## Docs
+
+	[Installación oficial](https://docs.djangoproject.com/en/1.5/intro/install/)
+	[Tutorial oficial](https://docs.djangoproject.com/en/1.5/intro/tutorial01/)
+	[Curso de django - maestrosdelweb.com](http://www.maestrosdelweb.com/guias/#guias-django)
+
+
+### Models and Forms
+
+	[django models attributes](http://www.maestrosdelweb.com/images/2012/05/django-models.jpg)
+	[django forms attributes](http://www.maestrosdelweb.com/images/2012/06/django-forms.pdf)
--- django_crud_example/settings.py
+++ django_crud_example/settings.py
@@ -1,5 +1,5 @@
 # Django settings for django_crud_example project.
-
+import os.path
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db.sqlite',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -29,11 +29,11 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Bogota'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
 SITE_ID = 1
 
@@ -111,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates').replace('\\', '/')
 )
 
 INSTALLED_APPS = (
@@ -121,9 +122,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'article'
 )
 
 # A sample logging configuration. The only tangible logging
